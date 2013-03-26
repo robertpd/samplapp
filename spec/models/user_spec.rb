9776517401270fff5ea582f2e39f2b965d8e3f08
rob <robertofdooley@gmail.com>
2013-03-26 19:36:26 +0000

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,14 +12,20 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example", email: "rubicon@gmail.com", password: "foogee", password_confirmation: "foogee")}
+#  before { @user = User.new(name: "Example", email: "rubicon@gmail.com", password: "foogee", password_confirmation: "foogee")}
   
-  subject {@user }
+before do
+  @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+end
+
+  subject { @user }
   it { should respond_to (:name)}
   it { should respond_to (:email)}
   it { should respond_to(:password_digest)} 
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)}
+  it { should respond_to(:authenticate)}
 
   it { should be_valid }
 

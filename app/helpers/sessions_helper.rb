1c4c43309b01ce147ec47ec8a7030c0cf32afbaa
rob <robertofdooley@gmail.com>
2013-04-01 12:35:12 +0100

    sync

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    current_user = user
   end
 
   def sign_out

bef119355be6e2813772fae91c1dbc962b861eb4
rob <robertofdooley@gmail.com>
2013-05-28 15:42:24 +0100

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,15 +2,18 @@ class StaticPagesController < ApplicationController
 
   def home
     if signed_in?
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
   def help
   end
+
   def about
   end
+  
   def contacts
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,11 +12,11 @@
 require 'spec_helper'
 
 describe User do
-#  before { @user = User.new(name: "Example", email: "rubicon@gmail.com", password: "foogee", password_confirmation: "foogee")}
-  
-before do
-  @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
-end
+  #  before { @user = User.new(name: "Example", email: "rubicon@gmail.com", password: "foogee", password_confirmation: "foogee")}
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
   it { should respond_to(:name)}
@@ -33,118 +33,169 @@ end
   it { should be_valid }
   it { should_not be_admin }
 
-  describe "micropost associations" do
-    before { @user.save }
+  it { should respond_to(:feed)}
+  it { should respond_to(:relationships)}
+  it { should respond_to(:followed_users)}
+  it { should respond_to(:following?)}
+  it { should respond_to(:follow!)}
 
-    let!(:older_micropost ) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-    end
-    let!(:newer_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-    end
-
-    it "should destroy associated microposts" do
-      microposts = @user.microposts.dup
-      @user.destroy
-      microposts.should_not be_empty
-      microposts.each do |micropost|
-        Micropost.find_by_id(micropost.id).should be_nil
-      end
-    end
-
-    it "should have the right microposts in the right order" do
-      @user.microposts.should == [newer_micropost, older_micropost]
-    end
-
-    describe "status" do
-      let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      end
-
-      its(:feed) { should include(newer_micropost) }
-      its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post) }
-    end
-  end
-
-  describe "with admin attribute set to 'true'" do
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user)}
     before do
-      @user.save!
-      @user.toggle!(:admin)
+      @user.save
+      @user.follow!(other_user)
     end
-    it { should be_admin}
+
+    it { should be_following(other_user)}
+    its (:followed_users) { should include(other_user)}
+
+    describe "followed_user" do
+      subject { other_user}
+      its(:followers){ should include(@user)}
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its (:followed_users) { should_not include(other_user)}
+    end
+end
+
+describe "micropost associations" do
+  before { @user.save }
+
+  let!(:older_micropost ) do
+    FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
   end
-  describe "when name is not present" do
-    before { @user.name = "" }
-    it { should_not be_valid }
+  let!(:newer_micropost) do
+    FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
   end
 
-  describe "when email is not present" do
-    before { @user.email = " "}
-    it { should_not be_valid }
-  end
-
-  describe "when name is too long" do
-    before { @user.name = "a"*51}
-    it { should_not be_valid }
-  end
-
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      address = %w[user@foo,com user_at_foo.org example.user@foo.]
-      address.each do |valid_address|
-        @user.email = valid_address
-        @user.should_not be_valid
-      end
+  describe "status" do
+    let(:unfollowed_post) do
+      FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
     end
-  end
-  describe "when email format is valid do" do
-    it "should be valid" do
-      address = %w[user@foo.com another@another.com]
-      address.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end
-    end
-  end
-  describe "when email address is already taken" do
+    let(:followed_user) { FactoryGirl.create(:user)}
+
     before do
-     user_with_same_email = @user.dup
-     user_with_same_email.email = @user.email.upcase
-     user_with_same_email.save
+      @user.follow!(followed_user)
+      3.times { followed_user.microposts.create!(content: "Lorem ipsum")}
     end
 
-    it { should_not be_valid }
+    its(:feed) { should include(newer_micropost)}
+    its(:feed) { should include(older_micropost)}
+    its(:feed) { should_not include(unfollowed_post)}
+    its (:feed) do
+      followed_user.microposts.each do |micropost|
+        should include(micropost)
+      end
+    end
   end
-  describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " "}
-    it { should_not be_valid }
+
+
+  it "should destroy associated microposts" do
+    microposts = @user.microposts.dup
+    @user.destroy
+    microposts.should_not be_empty
+    microposts.each do |micropost|
+      Micropost.find_by_id(micropost.id).should be_nil
+    end
   end
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatched"}
-    it { should_not be_valid }
+
+  it "should have the right microposts in the right order" do
+    @user.microposts.should == [newer_micropost, older_micropost]
   end
-  describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
-        it { should_not be_valid }
-  end
-  describe "return value of auth method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email)}
-    
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password)}
+
+  describe "status" do
+    let(:unfollowed_post) do
+      FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
     end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid")}
-      
-      it {should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
+    its(:feed) { should include(newer_micropost) }
+    its(:feed) { should include(older_micropost) }
+    its(:feed) { should_not include(unfollowed_post) }
+  end
+end
+
+describe "with admin attribute set to 'true'" do
+  before do
+    @user.save!
+    @user.toggle!(:admin)
+  end
+  it { should be_admin}
+end
+describe "when name is not present" do
+  before { @user.name = "" }
+  it { should_not be_valid }
+end
+
+describe "when email is not present" do
+  before { @user.email = " "}
+  it { should_not be_valid }
+end
+
+describe "when name is too long" do
+  before { @user.name = "a"*51}
+  it { should_not be_valid }
+end
+
+describe "when email format is invalid" do
+  it "should be invalid" do
+    address = %w[user@foo,com user_at_foo.org example.user@foo.]
+    address.each do |valid_address|
+      @user.email = valid_address
+      @user.should_not be_valid
     end
   end
-    describe "with a password that's too short" do
-      before { @user.password = @user.password_confirmation = "a" * 5 }
-      it { should be_invalid}
+end
+describe "when email format is valid do" do
+  it "should be valid" do
+    address = %w[user@foo.com another@another.com]
+    address.each do |valid_address|
+      @user.email = valid_address
+      @user.should be_valid
     end
+  end
+end
+describe "when email address is already taken" do
+  before do
+    user_with_same_email = @user.dup
+    user_with_same_email.email = @user.email.upcase
+    user_with_same_email.save
+  end
+
+  it { should_not be_valid }
+end
+describe "when password is not present" do
+  before { @user.password = @user.password_confirmation = " "}
+  it { should_not be_valid }
+end
+describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatched"}
+  it { should_not be_valid }
+end
+describe "when password confirmation is nil" do
+  before { @user.password_confirmation = nil }
+  it { should_not be_valid }
+end
+describe "return value of auth method" do
+  before { @user.save }
+  let(:found_user) { User.find_by_email(@user.email)}
+
+  describe "with valid password" do
+    it { should == found_user.authenticate(@user.password)}
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid")}
+
+    it {should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+end
+describe "with a password that's too short" do
+  before { @user.password = @user.password_confirmation = "a" * 5 }
+  it { should be_invalid}
+end
 end
